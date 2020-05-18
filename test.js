2859d3fd979cbff0c75eb10e4bf4ab9c66e4b990
Max Base <MaxBaseCode@Gmail.Com>
2020-05-18 11:30:10 +0430

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,14 @@ const namifyUrl= require(".")
 var ok=0
 var er=0
 var i=1
+
+function test(output, expect) {
+	console.log("[Test "+i+"]: "+output)
+	if(output == expect) {
+		ok++
+	}
+	else {
+		er++
+	}
+	i++
+}
