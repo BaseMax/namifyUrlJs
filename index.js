cd1cd676f88f3d8942ffe6b045e6ce6f5dec9b64
Max Base <MaxBaseCode@Gmail.Com>
2020-05-18 11:28:51 +0430

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 // https://github.com/BaseMax/namifyUrlJs
 'use strict'
 const namifyUrl = input => {
+	if(input.substr(0, 7) == "http://") { 
+		input=input.substr(7, input.length);
+		// input=input.replace("http://", '')
+	}
+	else if(input.substr(0, 8) == "https://") { 
+		input=input.substr(8, input.length);
+		// input=input.replace("https://", '')
+	}
+	if(input.substr(0, 4) == "www.") { 
+		input=input.substr(4, input.length);
+		// input=input.replace("www.", '')
+	}
+	if(input.substr(-1) == "/") {
+		input=input.substring(0, input.length-1);
+	}
 	return input
 };
 
